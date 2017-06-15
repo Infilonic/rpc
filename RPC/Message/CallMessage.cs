e28e0e379bc7ccafa2d57f2d06f87ff31f1aafd8
Sheggy <admin@sheggies.(none)>
2017-06-15 16:49:52 +0200

    Correct misspelled namespace

--- RPC/Message/CallMessage.cs
+++ RPC/Message/CallMessage.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RPC.MasterMessage
+namespace RPCMaster.Message
 {
     public class CallMessage : IProcedureMessage
     {
