24a3b75be662c4d763704fe684cf42c3dae602e1
Infilonic <admin@local>
2017-06-22 11:16:36 +0200

    Add AbstractMessage Class

---
+++ RPCMaster/Message/AbstractMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace RPCMaster.Message {
+	public abstract class AbstractMessage : IProcedureMessage<AbstractMessage> {
+
+		public AbstractMessage() { }
+		public virtual string Serialize() {
+			string serializedObject;
+			XmlSerializer serializer = new XmlSerializer(typeof(AbstractMessage));
+			using (StringWriter sWriter = new StringWriter()) {
+				serializer.Serialize(sWriter, this);
+				serializedObject = sWriter.ToString();
+			}
+			return serializedObject;
+		}
+
+		public static AbstractMessage Deserialize(string serializedObject) {
+			AbstractMessage deserializedObject;
+			XmlSerializer serializer = new XmlSerializer(typeof(AbstractMessage));
+			using (StringReader sReader = new StringReader(serializedObject)) {
+				deserializedObject = (AbstractMessage) serializer.Deserialize(sReader);
+			}
+			return deserializedObject;
+		}
+	}
+}
